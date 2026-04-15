ffe26e62a362e8dadcf7f86c3125268c1434af66
mrs-coder58 <nesibealacck@gmail.com>
2026-04-16 00:34:43 +0300

    Harmonik Sayı kodu eklendi.

---
+++ loops/HarmonikSayi.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class HarmonikSayi {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("N sayısını giriniz: ");
+        int n = scanner.nextInt();
+        double result = 0.0;
+
+        for(int i=1;i<=n;i++){
+            result += (1.0/i);
+        }
+
+        System.out.print("Sonuç: " + result);
+    }
+}
